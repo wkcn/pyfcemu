612e194e1819d54bc6a190c4a348cb28bece1d5d
wkcn <wkcn@live.cn>
2017-10-16 20:24:20 +0800

    improve gameview

--- gameview.py
+++ gameview.py
@@ -1,4 +1,5 @@
 from OpenGL.GL import *
+from numpy import *
 from view import *
 import glfw
 
@@ -8,7 +9,7 @@ class GameView(View):
         self.console = console
         self.title = title
         self._hash = _hash
-        self.texture = None
+        self.texture = self.createTexture()
         self.record = False
         self.frames = None
     def Enter(self):
@@ -41,5 +42,43 @@ class GameView(View):
         glBindTexture(GL_TEXTURE_2D, 0)
         #if self.record:
         #    self.frames.append(self.frames, console.Buffer().copy())
+
     def onKey(self, window, key, scancode, action, mods):
         pass
+
+    def createTexture(self):
+        texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glBindTexture(GL_TEXTURE_2D, 0)
+        return texture
+
+    def setTexture(self, im):
+        shape = im.shape
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, shape[0], shape[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, im)
+
+    def drawBuffer(self, window):
+        w, h = glfw.get_framebuffer_size(window)
+        s1 = float(w) / 256
+        s2 = float(h) / 240
+        padding = 0.0
+        f = 1.0 - padding
+        if s1 >= s2:
+            x = f * s2 / s1
+            y = f
+        else:
+            x = f
+            y = f * s1 / s2
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 1)
+        glVertex2f(-x, -y)
+        glTexCoord2f(1, 1)
+        glVertex2f(x, -y)
+        glTexCoord2f(1, 0)
+        glVertex2f(x, y)
+        glTexCoord2f(0, 0)
+        glVertex2f(-x, y)
+        glEnd()
